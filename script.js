9950ff88f59f81527a9327b74cc2c450c1e92f32
Kuwar20 <kuwarx1@gmail.com>
2023-12-25 18:10:04 +0530

    Update QR code generation functionality

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ let imgBox = document.getElementById('imgBox');
 let qrImage = document.getElementById('qrImage');
 let qrText = document.getElementById('qrText');
 
-function generateQr() {
-    qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    imgBox.classList.add('show-img')
-}
 
+function generateQr() {
+    if(qrText.value.length > 0){
+        qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add('show-img')
+    }else{
+        qrText.classList.add('error');
+        setTimeout(() => {
+            qrText.classList.remove('error');
+        }, 1000);
+    }
+    
+}
+ // this function will be called when the user press enter key in the input field
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        generateQr();
+    }
+}
